607682da0898686b83a800eeed273edab5231f2a
song-yeji <22100396@handong.ac.kr>
2022-09-08 14:24:27 +0900

    Update WordCRUD Class

--- src/main/java/WordCRUD.java
+++ src/main/java/WordCRUD.java
@@ -1,15 +1,20 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class WordCRUD implements ICRUD {
     ArrayList<Word> list;
     Scanner s = new Scanner(System.in);
+    final String fname = "Dictionary.txt";
 
 
-    WordCRUD(Scanner s){
+    WordCRUD(Scanner s) {
         list = new ArrayList<>();
         this.s = s;
     }
+
     @Override
     public Object add() {
         System.out.print("=> 난이도(1,2,3) & 새 단어 입력 : ");
@@ -22,33 +27,115 @@ public class WordCRUD implements ICRUD {
         return new Word(0, level, word, meaning);
     }
 
-    public void addWord(){
-        Word one = (Word)add();
+    public void addItem() {
+        Word one = (Word) add();
         list.add(one);
         System.out.println("\n새 단어가 단어장에 추가되었습니다!!!\n");
     }
 
     @Override
-    public int update(Object obj){
+    public int update(Object obj) {
         return 0;
     }
 
     @Override
-    public int delete(Object obj){
+    public int delete(Object obj) {
         return 0;
     }
 
     @Override
-    public void selectOne(int id){
+    public void selectOne(int id) {
 
     }
 
-    public void listAll(){
+    public void listAll() {
         System.out.println("--------------------------------");
-        for(int i = 0; i<list.size(); i++){
-            System.out.print(i+1 + " ");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(i + 1 + " ");
             System.out.println(list.get(i).toString());
         }
         System.out.println("--------------------------------");
+    }
+
+    public ArrayList<Integer> listAll(String keyword) {
+        ArrayList<Integer> idlist = new ArrayList<>();
+        int j = 0;
+        System.out.println("--------------------------------");
+        for (int i = 0; i < list.size(); i++) {
+            String word = list.get(i).getWord();
+            if (!word.contains(keyword)) continue;
+            System.out.print(j + 1 + " ");
+            System.out.println(list.get(i).toString());
+            idlist.add(i);
+            j++;
+        }
+        System.out.println("--------------------------------");
+
+        return idlist;
+    }
+
+    public void updateItem() {
+        System.out.print("=> 수정할 단어 검색 : ");
+        String keyword = s.next();     // 공백 허용 x
+        ArrayList<Integer> idlist = this.listAll(keyword);
+
+        System.out.print("=> 수정할 번호 선택 : ");
+        int id = s.nextInt();
+        s.nextLine();                  // 개행문자 제거
+
+        System.out.print("=> 뜻 입력 : ");
+        String meaning = s.nextLine();
+        Word word = list.get(idlist.get(id - 1));
+        word.setMeaning(meaning);
+
+        System.out.println("\n단어가 수정되었습니다. \n");
+    }
+
+    public void deleteItem() {
+        System.out.print("=> 삭제할 단어 검색 : ");
+        String keyword = s.next();     // 공백 허용 x
+        ArrayList<Integer> idlist = this.listAll(keyword);
+
+        System.out.print("=> 삭제할 번호 선택 : ");
+        int id = s.nextInt();
+        s.nextLine();                  // 개행문자 제거
+
+        System.out.print("=> 정말로 삭제하실래요(Y/n) : ");
+        String ans = s.next();
+
+        if (ans.equalsIgnoreCase("y")) {
+            list.remove((int) idlist.get(id - 1));
+            System.out.println("\n단어가 삭제되었습니다. \n");
+        } else {
+            System.out.println("\n취소되었습니다. \n");
+        }
+    }
+
+    public void loadFile() {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fname));
+            String line;
+            int count = 0;
+
+            while(true){
+                line = br.readLine();
+                if(line == null) break;
+
+                String data[] = line.split("\\|");
+                int level = Integer.parseInt(data[0]);
+                String word = data[1];
+                String meaning = data[2];
+
+                list.add(new Word(0, level, word, meaning));
+                count++;
+            }
+
+            br.close();
+            System.out.println("\n==> " + count + "개 로딩 완료!!!\n");
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
